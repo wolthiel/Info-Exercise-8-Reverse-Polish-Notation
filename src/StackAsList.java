68758b6df99c9e173525363d142fa8ad95d37726
wolthiel <82994042+wolthiel@users.noreply.github.com>
2021-06-04 04:27:03 +0200

    Update StackAsList.java

--- src/StackAsList.java
+++ src/StackAsList.java
@@ -1,28 +1,112 @@
+import java.util.NoSuchElementException;
 
-public class StackAsList implements Stack{
+public class StackAsList {
 	public Node first;
+	public int totalNumberOfNodes = 0;
 	
-	public StackAsList() {}
-	public StackAsList(Node n) {
-		first = n;
+	public void addFirstNode(Object object) {
+		Node last = new Node(object, first);
+		first = last;
+		totalNumberOfNodes ++;
 	}
-	
-	@Override
-	public void push(String elem) {
+	public void printAllNodes() {
+		Node currentNode = first;		
+		while (currentNode.next != null)
+		{
+			System.out.println(currentNode.data);
+			currentNode = currentNode.next;
+		}
+		if (currentNode.next == null) {
+			System.out.println(currentNode.data);
+		}
+	}
+	public Object removeFirst() {
+		if(first == null) { throw new NoSuchElementException();}
+		Object object = first.data;
+		first = first.next;
+		return object;
+	}
+	public StackAsList() {
+		first = null;
+	}
+	public StackListIterator listIterator() {
+		return new StackListIterator();
 	}
 	
 	private class Node{
 		Object data;
 		Node next;
-		public Node (Object object) {
+		private Node() {
+		}
+		private Node(Object object) {
 			data = object;
 		}
-		public Node (Object object, Node next) {
+		private Node(Object object, Node nextNode) {
 			data = object;
-			this.next = next;
+			next = nextNode;
 		}
-		
-		
 	}
+	class StackListIterator {
+		private Node position;
+		private Node previous;
+		private boolean isAfterNext;
+		
+		public StackListIterator() {
+			position = null;
+			previous = null;
+			isAfterNext = false;
+		}
+		public Object next() {
+			if (!hasNext()) { throw new NoSuchElementException();}
+			previous = position;
+			isAfterNext = true;
+			
+			if (position == null){
+				position = first; 
+			}
+			else {
+				position = position.next;
+			}
+			return position.data;
+		}
+		public boolean hasNext() {
+			if (position == null) {
+				return first != null;
+			}
+			else {
+				return position.next != null;
+			}
+		}
+		public void add(Object object) {
+			if (position == null) {
+				addFirstNode(object);
+				position = first;
+			}
+			else {
+				Node newNode = new Node(object, position.next);
+				position.next = newNode;
+				position = newNode;
+			}
+			totalNumberOfNodes ++;
+			isAfterNext = false;
+		}
+		public void remove() {
+			if(!isAfterNext) { throw new IllegalStateException();}
+			if(position == first) {
+				removeFirst();
+			}
+			else {
+				previous.next = position.next;
+			}
+			position = previous;
+			totalNumberOfNodes --;
+			isAfterNext = false;
+		}
+		public void set(Object object) {
+			if(!isAfterNext) { throw new IllegalStateException();}
+			position.data = object;
+		}
+	}
+	
 
 }
