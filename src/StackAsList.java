9ec6323455371a18d5eba0c9cae31bb719170c87
wolthiel <82994042+wolthiel@users.noreply.github.com>
2021-06-04 10:48:15 +0200

    Update StackAsList.java

--- src/StackAsList.java
+++ src/StackAsList.java
@@ -4,11 +4,16 @@ public class StackAsList {
 	public Node first;
 	public int totalNumberOfNodes = 0;
 	
+	// Fügt an der ersten Stelle des Stacks ein Objekt hinzu
 	public void addFirstNode(Object object) {
 		Node last = new Node(object, first);
 		first = last;
 		totalNumberOfNodes ++;
 	}
+	
+	// Druckt alle Objekte im Node aus.
+	// Startet mit der ersten Position.
+	// Jedes Objekt erhält eine neue Zeile.
 	public void printAllNodes() {
 		Node currentNode = first;		
 		while (currentNode.next != null)
@@ -20,19 +25,26 @@ public class StackAsList {
 			System.out.println(currentNode.data);
 		}
 	}
+	
+	// Entfernt das Objekt an der ersten Position im Stack.
 	public Object removeFirst() {
 		if(first == null) { throw new NoSuchElementException();}
 		Object object = first.data;
 		first = first.next;
 		return object;
 	}
+	
+	// Initialisiert den Stack ohne Inhalt.
 	public StackAsList() {
 		first = null;
 	}
+	
+	// Erstellt einen Listen-Iterator
 	public StackListIterator listIterator() {
 		return new StackListIterator();
 	}
 	
+	// Definiert die Klasse "Node"
 	private class Node{
 		Object data;
 		Node next;
@@ -46,16 +58,21 @@ public class StackAsList {
 			next = nextNode;
 		}
 	}
+	
+	// Definiert die Klasse "StackListIterator"
 	class StackListIterator {
 		private Node position;
 		private Node previous;
 		private boolean isAfterNext;
 		
+		// Initialisiert den Iterator bevore der ersten Stelle.
 		public StackListIterator() {
 			position = null;
 			previous = null;
 			isAfterNext = false;
 		}
+		
+		// Bewegt den Iterator zur nachfolgenden Stelle.
 		public Object next() {
 			if (!hasNext()) { throw new NoSuchElementException();}
 			previous = position;
@@ -69,6 +86,8 @@ public class StackAsList {
 			}
 			return position.data;
 		}
+		
+		// Boolean, der prüft, ob das ausgewählte Objekt ein nachfolgendes hat
 		public boolean hasNext() {
 			if (position == null) {
 				return first != null;
@@ -77,6 +96,8 @@ public class StackAsList {
 				return position.next != null;
 			}
 		}
+		
+		// Fügt ein Objekt an der ausgewählten Stelle hinzu
 		public void add(Object object) {
 			if (position == null) {
 				addFirstNode(object);
@@ -90,6 +111,8 @@ public class StackAsList {
 			totalNumberOfNodes ++;
 			isAfterNext = false;
 		}
+		
+		// Entfernt ein Objekt an der ausgewählten Stelle
 		public void remove() {
 			if(!isAfterNext) { throw new IllegalStateException();}
 			if(position == first) {
@@ -102,6 +125,8 @@ public class StackAsList {
 			totalNumberOfNodes --;
 			isAfterNext = false;
 		}
+		
+		// Verändert das Objekt an der ausgewählten Stelle
 		public void set(Object object) {
 			if(!isAfterNext) { throw new IllegalStateException();}
 			position.data = object;
