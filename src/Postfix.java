15319ccef8aa6bbd29ca94c9d6bacdd38f5a120e
AstnMartin <82873818+AstnMartin@users.noreply.github.com>
2021-06-05 17:43:30 +0200

    Update Postfix.java

--- src/Postfix.java
+++ src/Postfix.java
@@ -1,10 +1,41 @@
+import java.util.Stack;
 
 public class Postfix {
+	
+	public int evaluate(String pfx) {		
+		Stack<Object> stackforDigits = new Stack<Object>();
+		int result = 0;
+		int firstdigit = 0;
+		int seconddigit = 0;
 
-	public int evaluate (String pfx) {
+		char[] c = pfx.toCharArray();
 		
-		
+		for (char chr: c) {
+			if(Character.isDigit(chr)){
+				stackforDigits.push(chr);	
+			} else if (!(Character.isDigit(chr))){
+				
+				firstdigit = Integer.parseInt(stackforDigits.pop().toString());
+				seconddigit = Integer.parseInt(stackforDigits.pop().toString());
+				
+				switch(chr) {
+				case '+':
+					result = seconddigit+firstdigit;
+					break;
+				case '/':
+					result = seconddigit/firstdigit;
+					break;
+				case '-':	
+					result = seconddigit-firstdigit;
+					break;
+				case '*' :
+					result = seconddigit*firstdigit;
+					break;	
+				}
+				stackforDigits.push(result);
+				}
+		}
+		return result;
 	}
 
-	
 }
