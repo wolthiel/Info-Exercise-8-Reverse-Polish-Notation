7cb525c719459ba0d10e5e06a517397545e1a269
wolthiel <82994042+wolthiel@users.noreply.github.com>
2021-06-05 22:30:11 +0200

    Update Postfix.java

--- src/Postfix.java
+++ src/Postfix.java
@@ -39,55 +39,6 @@ public class Postfix {
 		}
 		return result;
 	}
-	
-	public static char makeObjectToChar(){
-		StackAsList stackforDigits = new StackAsList();
-		String string = stackforDigits.peekAsString();
-		char[] ch = new char[string.length()];
-		for (int i = 0; i < string.length(); i++) {
-			ch[i] = string.charAt(i);
-			}
-		for (char character : ch) {
-			return character;
-		}
-		return 0;
-	}
-
-	      
-	    // A utility function to return 
-	    // precedence of a given operator
-	    // Higher returned value means 
-	    // higher precedence
-	    public static int Prec(char ch)
-	    {
-	        switch (ch)
-	        {
-	        case '+':
-	        case '-':
-	            return 1;
-	       
-	        case '*':
-	        case '/':
-	            return 2;
-	       
-	        case '^':
-	            return 3;
-	        }
-	        return -1;
-	    }
-	       
-	    // The main method that converts 
-	    // given infix expression
-	    // to postfix expression.
-	    /* neue Var für opperator
-	     * 1: loop für jedes Element im Stack
-	     * 2: in loop switch-statement -> switch für opperatoren + defaultwert für alles andere defwert wird in anderen Stack gegeben
-	     * 3:am ende wird der opperator hinzugefügt 
-	     * 
-	     */
-	    
-	    
-	    
 	    public static String infixToPostfix(String exp)
 	    {
 	    	char operator = 0;
